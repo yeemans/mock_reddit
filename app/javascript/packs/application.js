28cac698e07402ccdcc6cf9b76ab25eaa392f51b
yeemans <arthur.roth.07@gmail.com>
2022-06-18 22:10:13 -0700

    removed requiring of react

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,9 +3,6 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-//= require react
-//= require react_ujs
-//= require components
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
