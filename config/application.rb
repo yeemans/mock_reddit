54d54572a3525a241b53e5a64fb35ff583350a40
yeemans <arthur.roth.07@gmail.com>
2022-06-18 22:26:36 -0700

    switched code loader to classic

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Reddit
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.autoloader = :classic
 
     # Configuration for the application, engines, and railties goes here.
     #
