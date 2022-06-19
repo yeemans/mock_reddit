e2065aff073816ef5af3155558033851a631b3cd
yeemans <arthur.roth.07@gmail.com>
2022-06-18 22:15:05 -0700

    trying again to remove react

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,6 +28,4 @@ ActiveStorage.start()
 
 require("trix")
 require("@rails/actiontext")// Support component names relative to this directory:
-var componentRequireContext = require.context("components", true);
-var ReactRailsUJS = require("react_ujs");
-ReactRailsUJS.useContext(componentRequireContext);
+
